4181167090c1178cb04f4efd4b8296b4b3f72abf
Raissa Coelho <72715305+raissa-coelho@users.noreply.github.com>
2022-10-30 18:59:11 -0300

    Update Cache.java

--- src/main/memoria/Cache.java
+++ src/main/memoria/Cache.java
@@ -6,6 +6,9 @@ import static main.util.RuntimeData.*;
 import static main.util.Util.getRandom;
 import static main.util.Util.log2;
 
+/**
+ * Classe Cache
+ */
 public class Cache {
 
     private int assoc;
@@ -18,7 +21,14 @@ public class Cache {
     private int hit_count = 0;
     private int miss_count = 0;
     private ArrayList<Bloco> blocos;
-
+    
+    /**
+     * 
+     * @param nset
+     * @param bsize
+     * @param assoc
+     * @param sub
+     */
     public Cache(int nset, int bsize, int assoc, int sub) {
         this.setOffset(log2(bsize));
         this.setIndice(log2(nset));
@@ -29,12 +39,23 @@ public class Cache {
         setBlocos(new ArrayList<>(capacidade));
         initBlocos(assoc, nset);
     }
-
+    
+    /**
+     * 
+     * @param assoc
+     * @param nset
+     */
     private void initBlocos(int assoc, int nset){
         for(int i = 0; i < assoc * nset; i++){
             blocos.add(i, new Bloco(getIndice(), getOffset()));
         }
     }
+    
+    /**
+     * 
+     * @param address
+     * @return
+     */
     private int[] decode(int address) {
         int[] retVal = new int[4];
         retVal[0] = address; // address
@@ -44,7 +65,11 @@ public class Cache {
         return retVal;
     }
 
-    //Retorna falso caso já tenha algo na cache
+    /**
+     * 
+     * @param endereco recebe endereco
+     * @return retorna falso caso já tenha algo na cache
+     */
     private boolean read(int endereco) {
         int[] args = decode(endereco);
         Bloco bloco = blocos.get(args[3]);
@@ -62,14 +87,13 @@ public class Cache {
 
     /**
      * Método de para escrita em cache
-     *
+     * Possiveis retornos
+     * 0 - Escrita com erro compulsorio
+     * 1 - Escrita com erro de conflito
+     * 2 - Escrita com erro de capacidade
      * @param endereco endereco para a realizacao da leitura
      * @return resultado da leitura
      */
-    //Possiveis retornos
-    //0 - Escrita com erro compulsorio
-    //1 - Escrita com erro de conflito
-    //2 - Escrita com erro de capacidade
     private int write(int endereco) {
         if (getBlocos() != null) {
             for (Bloco bloco : getBlocos()) {
@@ -113,57 +137,112 @@ public class Cache {
         }
         return -1;
     }
-
+    
+    /**
+     *  
+     * @param endereco
+     */
     public void find(int endereco) {
         if (!read(endereco)) {
             write(endereco);
         }
     }
-
+    
+    /**
+     * 
+     * @return o valor da associatividade
+     */
     public int getAssoc() {
         return assoc;
     }
-
+    
+    /**
+     * 
+     * @param assoc valor da associatividade
+     */
     public void setAssoc(int assoc) {
         this.assoc = assoc;
     }
-
+    
+    /**
+     * 
+     * @return o offset
+     */
     public int getOffset() {
         return offset;
     }
-
+    
+    /**
+     * 
+     * @param offset
+     */
     public void setOffset(int offset) {
         this.offset = offset;
     }
-
+    
+    /**
+     * 
+     * @return o valor da tag
+     */
     public int getTag() {
         return tag;
     }
-
+    
+    /**
+     * 
+     * @param tag
+     */
     public void setTag(int tag) {
         this.tag = tag;
     }
-
+    
+    /**
+     * 
+     * @return o valor do indice
+     */
     public int getIndice() {
         return indice;
     }
-
+    
+    /**
+     * 
+     * @param indice
+     */
     public void setIndice(int indice) {
         this.indice = indice;
     }
-
+    
+    /**
+     * 
+     * @return retorna o numero da politica de substituicao
+     * 1 -> RANDOM
+     * 2 -> FIFO
+     * 3 -> LRU
+     */
     public int getSub() {
         return sub;
     }
-
+    
+     /**
+     * 
+     * @param sub
+     */
     public void setSub(int sub) {
         this.sub = sub;
     }
-
+    
+    /**
+     * 
+     * @return blocos
+     */
     public ArrayList<Bloco> getBlocos() {
         return blocos;
     }
-
+    
+    /**
+     * 
+     * @param blocos
+     */
     public void setBlocos(ArrayList<Bloco> blocos) {
         this.blocos = blocos;
     }
